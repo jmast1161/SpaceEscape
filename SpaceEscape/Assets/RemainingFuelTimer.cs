335ee332a7118c850921d5940a53caa20c1a4585
Josh <jmast1161@gmail.com>
2020-06-14 00:03:18 -0400

    Add fuel timer

---
+++ SpaceEscape/Assets/RemainingFuelTimer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RemainingFuelTimer : MonoBehaviour
+{
+    private float remainingFuel = 5;
+    private Text fuelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuelText = GetComponent<Text>();        
+        fuelText.text = "Remaining Fuel: " + Mathf.Round(remainingFuel);        
+        GameEvents.Current.OnPlayerFuelPickup += OnPlayerFuelPickup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(remainingFuel > 0)
+        {
+            remainingFuel -= Time.deltaTime;
+            fuelText.text = "Remaining Fuel: " + Mathf.Round(remainingFuel);
+        }
+    }
+    
+    private void OnPlayerFuelPickup()
+    {
+        remainingFuel += 2;
+        fuelText.text = "Remaining Fuel: " + Mathf.Round(remainingFuel);
+    }
+}
