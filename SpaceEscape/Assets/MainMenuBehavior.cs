639a03120d7bff46b4bcf116ab5b60e770192598
Josh <jmast1161@gmail.com>
2020-07-12 16:25:49 -0400

    Main menu working

---
+++ SpaceEscape/Assets/MainMenuBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBehavior : MonoBehaviour
+{
+    private Button playButton;
+    private Button quitButton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        playButton.onClick.AddListener(Play);
+        quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+        quitButton.onClick.AddListener(Quit);
+
+        playButton.Select();
+    }
+
+    private void Play()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
